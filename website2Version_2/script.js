f5e2fca8924571e6462edcc6fbc4926885461bce
BasdDff <71138035+BasdDff@users.noreply.github.com>
2020-11-17 22:50:22 +0300

    Add files via upload

--- website2Version_2/script.js
+++ website2Version_2/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 		var target1 = jQuery(this).data('target'); //вызываем метод data со значением data-атрибута target в тег <button> передали атрибут data-target
 		jQuery(target1).toggleClass("navi-main-open"); //Метод toggleClass - добавляет или убирает класс navi-main-open, когда мы будем
 	}); //кликать по меню, и дальше в CSS описываем класс navi-main-open, чтобы когда он появлялся открывалось меню
-  
+			
 
   
 });
