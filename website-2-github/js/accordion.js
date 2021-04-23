9b9192793378cc0d2b062a0349c2067bdbe6df77
BasdDff <71138035+BasdDff@users.noreply.github.com>
2021-04-23 14:45:19 +0300

    Delete website-2-github directory

--- website-2-github/js/accordion.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	
-	//4. Створити ефект аккордіон.
-	$(".accordion-item_trigger").click(function(){
-		//$(this).next(".accordion-item_content").toggle(); //toggle отображает/скрывает (none или block)
-		$(this).next(".accordion-item_content").slideToggle(300);
-	});
-	
-});
--- website-2-github/js/sortable.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	
-	//5. За допомогою Sortable створити переміщувані контейнери; 
-	$(function() {
-		$(".sortable").sortable();
-		$(".sortable").disableSelection();
-	});
-	
-});
